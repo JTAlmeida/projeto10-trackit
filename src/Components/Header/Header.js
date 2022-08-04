2e7f29c03d4c06d0df011271c899f931acae7c2a
Jonatas <jonatasalmeidasm@gmail.com>
2022-08-04 11:24:41 -0300

    removed static image from header (also, last commit was already getting user image from localstorage)

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,10 @@
-import imgHeader  from "../../assets/imgheader.png"
 import { useContext } from 'react';
 import UserContext from "../../contexts/UserContext";
 import { HeaderWrapper } from "./Header.style";
 
 export default function Header() {
   const { user, setUser } = useContext(UserContext);
-  
+
     return (
       <HeaderWrapper>
         <h1>TrackIt</h1>
